eba4f33f7e4ff36b53703312f6281caa42a6871f
elpulpo0 <chris.elpulpo@gmail.com>
2025-08-19 19:00:51 +0200

    update traefik

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -4,4 +4,4 @@ const PORT_BACK: string = import.meta.env.VITE_PORT_BACK;
 export const backend_url =
   env === 'dev'
     ? `http://localhost:${PORT_BACK}`
-    : `http://77.37.51.76:${PORT_BACK}`;
+    : `https://api.badarts.fr`;
