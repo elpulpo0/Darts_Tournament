1629417dd391683f1d9b732e323a33601afb98aa
elpulpo0 <chris.elpulpo@gmail.com>
2025-08-19 17:04:07 +0200

    update config

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,8 +1,7 @@
 const env = import.meta.env.VITE_ENV;
-const PORT_FRONT: string = import.meta.env.VITE_PORT;
 const PORT_BACK: string = import.meta.env.VITE_PORT_BACK;
 
 export const backend_url =
   env === 'dev'
     ? `http://localhost:${PORT_BACK}`
-    : `http://77.37.51.76:${PORT_FRONT}`;
+    : `http://77.37.51.76:${PORT_BACK}`;
