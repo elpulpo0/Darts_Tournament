31e4c1584230d770025f33748973c04592f26a33
elpulpo0 <chris.elpulpo@gmail.com>
2025-08-19 16:50:31 +0200

    update config

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,7 +1,8 @@
 const env = import.meta.env.VITE_ENV;
+const PORT_FRONT: string = import.meta.env.VITE_PORT;
 const PORT_BACK: string = import.meta.env.VITE_PORT_BACK;
 
 export const backend_url =
   env === 'dev'
     ? `http://localhost:${PORT_BACK}`
-    : 'https://dashboard-api.microphyt.net';
+    : `http://77.37.51.76:${PORT_FRONT}`;
