46c07fdea13fb7962163990ec77dad064727766e
elpulpo0 <chris.elpulpo@gmail.com>
2025-10-16 13:43:42 +0200

    fix

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-chown -R appuser:appuser /app/logs /app/leaderboard /app/database /app/backups 2>/dev/null || true
+chown -R appuser:appuser /app/logs /app/leaderboards /app/database /app/backups 2>/dev/null || true
 
 exec su-exec appuser "$@"
