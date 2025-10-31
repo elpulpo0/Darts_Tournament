840ca548f58e35dc319aa783db032b1819727b9f
elpulpo0 <chris.elpulpo@gmail.com>
2025-10-31 14:13:21 +0100

    fix: correct case for organiser link "la fléchette de salm"

--- frontend/src/pages/OfficialCalendar.vue
+++ frontend/src/pages/OfficialCalendar.vue
@@ -201,7 +201,7 @@ const organiserLinks: Record<string, string> = {
     "pouss'in the darts - la fléchette sanfloraine": "https://www.facebook.com/LaFlechetteSanfloraine",
     "droit aux bulls": "https://www.instagram.com/droitobullsclub",
     "drap darts": "https://www.facebook.com/groups/1024078255149460",
-    "La fléchette de Salm": "https://www.facebook.com/LaFlechetteDeSalm88"
+    "la fléchette de salm": "https://www.facebook.com/LaFlechetteDeSalm88"
 };
 
 const getOrganiserLink = (organiser: string | undefined | null): string | undefined => {
